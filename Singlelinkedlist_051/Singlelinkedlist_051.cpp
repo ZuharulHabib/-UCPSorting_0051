b340da017871bde567060a2a1ed821fac7534703
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-04-26 14:54:08 +0700

    tampilan saat di jalankan

--- Singlelinkedlist_051/Singlelinkedlist_051.cpp
+++ Singlelinkedlist_051/Singlelinkedlist_051.cpp
@@ -143,3 +143,17 @@ int main() {
 			cin >> pilihan;
 			switch (pilihan)
 			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
