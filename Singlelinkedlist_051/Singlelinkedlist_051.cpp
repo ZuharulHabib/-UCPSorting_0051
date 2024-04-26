f99e1d689d185be8f632c1c0ef7ac7b2d96286ae
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-04-26 14:47:25 +0700

    implementasi dari fungsi deleteNode yang bertujuan untuk menghapus node dengan nomor mahasiswa tertentu dari linked list

--- Singlelinkedlist_051/Singlelinkedlist_051.cpp
+++ Singlelinkedlist_051/Singlelinkedlist_051.cpp
@@ -55,3 +55,30 @@ bool searchNode(int nim, Node* current, Node* previous) {
 	current = START;
 	while (current != NULL && nim > current->noMhs)
 	{
+		previous = current;
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (searchNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
